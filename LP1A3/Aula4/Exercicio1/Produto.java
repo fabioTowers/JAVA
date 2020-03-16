e19f3433116489beb43334a59cbd46b8fe23b192
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2020-03-16 14:53:35 -0300

    Create Produto.java
    Faça um programa console em Java que utilize os seguintes conceitos de programação orientada a objetos: classes, objetos, atributos e métodos. A aplicação deve possuir as seguintes classes: 
    • Produto – Classe que contém os atributos privados: codigo (int), descricao (String), preco (float) e ativo (boolean), além dos métodos públicos get e set. Os métodos get devem retornar os respectivos valores dos atributos da classe Produto. Os métodos set devem atribuir os valores recebidos como argumento aos respectivos atributos da classe. Inclua um construtor na classe Produto, para inicializar seus atributos. 
    • Inicio – Classe que contém o método Main. Deve ser criada uma instância Produto que chamará os métodos get e set da classe Produto. Use os métodos set para atribuir os valores informados pelo usuário aos atributos da classe Produto, e os métodos get para apresentar seus novos valores ao usuário.

---
+++ LP1A3/Aula4/Exercicio1/Produto.java
@@ -0,0 +1,49 @@
+package exercicio1;
+
+public class Produto {
+
+	private Integer codigo;
+	private String descricao;
+	private Float preco;
+	private Boolean ativo;
+	
+	public Produto() {
+		codigo = null;
+		descricao = null;
+		preco = null;
+		ativo = false;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+}
