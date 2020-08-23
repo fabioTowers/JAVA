26b9b6aebca494bf8d0c29c5422e49d452cfbe28
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2020-08-23 18:07:10 -0300

    Create Cliente.java

---
+++ LP1A3/Aula5/Exercicio2/Cliente.java
@@ -0,0 +1,22 @@
+package exercicio2;
+
+public class Cliente {
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private String email;
+	
+	public String getCliente() {
+		return("\n Nome: " + nome + 
+			   "\n CPF: " + cpf +
+			   "\n Email: " + email +
+			   "\n Endereco: " + endereco);
+	}
+	
+	public void setCliente(String nome, String cpf, String endereco, String email) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.endereco = endereco;
+		this.email = email;
+	}
+}
