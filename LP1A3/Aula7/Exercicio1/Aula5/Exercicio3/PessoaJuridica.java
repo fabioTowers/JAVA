14cc3d2841b343eb19c92195ba7770b6e1b376b7
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2020-09-03 15:32:55 -0300

    Create PessoaJuridica.java

---
+++ LP1A3/Aula7/Exercicio1/Aula5/Exercicio3/PessoaJuridica.java
@@ -0,0 +1,20 @@
+package exercicio3;
+
+public class PessoaJuridica extends Cliente{
+	private String razaoSocial;
+	private String cnpj;
+	
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+}
