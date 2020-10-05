0d4d96ebc26f89f9d69da04f85f6005240faafca
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2020-10-05 14:59:16 -0300

    Create Principal.java

---
+++ LP1A3/Aula11/Exercicio1/Principal.java
@@ -0,0 +1,21 @@
+import javax.swing.SwingUtilities;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * O método invokeLater da classe SwingUtilities é usado para que a instanciação
+		 * e exibição da janela sejam feitas de forma assíncrona (em elementos diferentes),
+		 * assim a JVM pode processar os eventos da janela de forma adequada*/
+		SwingUtilities.invokeLater( new Runnable()
+		{
+			@Override
+			public void run() { // Implementa o método run da interface Runnable
+				//Instância a janela e setVisible(true) exibe a janela
+				new JanelaExec1().setVisible(true);
+			}//método run
+		});//swing utilities
+	}//main
+
+}//classe
